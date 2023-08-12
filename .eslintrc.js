67a839d51dcb58b45ea25ecbfd51387a4e3f30d9
j.a.s <aras.strong8@gmail.com>
2023-08-12 20:43:32 -0300

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,13 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: ['airbnb-base', 'eslint:recommended', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  rules: {},
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 
 const usersRoutes = require('./routes/user_routes');

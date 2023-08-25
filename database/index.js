a3cb2b65dc4f630faaeacce943b43ca4a26170f8
j.a.s <aras.strong8@gmail.com>
2023-08-25 18:46:34 -0300

    renomeand

--- database/index.js
+++ database/index.js
@@ -1,12 +1,12 @@
 const Sequelize = require("sequelize")
 const dbConfig = require("./../config/database")
 
-const User = require('../src/models/usuarios');
+const Usuario = require('../src/models/usuarios');
 const Livro = require('../src/models/livros');
 
 const connection = new Sequelize(dbConfig);
 
-User.init(connection);
+Usuario.init(connection);
 Livro.init(connection);
 
 module.exports = connection;
